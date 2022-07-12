33ca9715ec2a396a38c1934d31a5b3839fe0ec04
Mateus <mateusc.rossetto@hotmail.com>
2022-07-11 23:44:29 -0300

    fix: get posts

--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -1,49 +1,55 @@
-import { useEffect, useState } from "react";
-import Publish from "../publish";
-import { Container, Load, Message } from "./style";
+import { useEffect, useState } from 'react';
+import Publish from '../publish';
+import { Container, Load, Message } from './style';
 import * as api from '../../services/api';
-import Post from "../post";
+import Post from '../post';
 import { ThreeDots } from 'react-loader-spinner';
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 import Swal from 'sweetalert2';
 
 export default function Timeline() {
+	const [posts, setPosts] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const { hashtag } = useParams();
 
-    const [posts, setPosts] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const { hashtag } = useParams();
+	useEffect(() => {
+		setIsLoading(true);
+		let promise = null;
+		if (hashtag) promise = api.getPosts(hashtag);
+		else promise = api.getPosts();
 
-    useEffect(() => {
-        setIsLoading(true);
-        let promise = null;
-        if(hashtag)
-        promise = api.getPosts(hashtag);
-        else
-        promise = api.getPosts();
-        
-        promise.then(response => {
-            let posts = response.data;
-            setPosts(posts);
-            setIsLoading(false);
-        }).catch(error => {
-            Swal.fire({
-                icon: 'error',
-                title: "OOPS...",
-                text: 'An error occured while trying to fetch the posts, please refresh the page',
-              });
-            setIsLoading(false);
-        });
-    }, [hashtag]);
+		promise
+			.then((response) => {
+				let posts = response.data;
+				console.log(posts);
+				setPosts(posts);
+				setIsLoading(false);
+			})
+			.catch((error) => {
+				Swal.fire({
+					icon: 'error',
+					title: 'OOPS...',
+					text: 'An error occured while trying to fetch the posts, please refresh the page',
+				});
+				setIsLoading(false);
+			});
+	}, [hashtag]);
 
-    return(
-        <Container>
-            <Publish />
-            {(posts.length === 0 && isLoading === false)? <Message>There are no posts yet</Message> : ""}
-            {isLoading? (
-                <Load><ThreeDots color="#FFFFFF" height={50} width={50} /></Load>
-            ) : (
-                posts.map((post, i) => <Post key={i} post={post} />)
-            )}
-        </Container>
-    );
+	return (
+		<Container>
+			<Publish />
+			{posts.length === 0 && isLoading === false ? (
+				<Message>There are no posts yet</Message>
+			) : (
+				''
+			)}
+			{isLoading ? (
+				<Load>
+					<ThreeDots color="#FFFFFF" height={50} width={50} />
+				</Load>
+			) : (
+				posts.map((post, i) => <Post key={i} post={post} />)
+			)}
+		</Container>
+	);
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,103 +1,101 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({ baseURL: process.env.REACT_APP_BASE_URL });
 
 const config = (token) => {
-    return {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }
-}
+	return {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	};
+};
 
 const signUp = (body) => {
-    const promise = api.post('/sign-up', body);
+	const promise = api.post('/sign-up', body);
 
-    return promise;
-}
+	return promise;
+};
 
 const login = (body) => {
-    const promise = api.post('/login', body);
-    return promise;
-}
+	const promise = api.post('/login', body);
+	return promise;
+};
 
 const getUserData = (data) => {
-    const promise = api.get(`/users/${data.id}`, config(data.token));
-    return promise;
-}
+	const promise = api.get(`/users/${data.id}`, config(data.token));
+	return promise;
+};
 
-const publishPost = ({token, publishData, id}) => {
-    const body = {userId: id, ...publishData};
-    const promise = api.post('/posts', body, config(token));
-    return promise;
-}
+const publishPost = ({ token, publishData, id }) => {
+	const body = { userId: id, ...publishData };
+	const promise = api.post('/posts', body, config(token));
+	return promise;
+};
 
 const getPosts = (hashtag) => {
-    let promise = null;
-    if(hashtag)
-    promise = api.get(`/posts?hashtag=${hashtag}`);
-    else
-    promise = api.get('/posts');
-    return promise;
-}
+	let promise = null;
+	if (hashtag) promise = api.get(`/posts?hashtag=${hashtag}`);
+	else promise = api.get('/posts');
+	return promise;
+};
 
 const likePost = (postId, userId, token) => {
-    const body = {
-        postId,
-        userId
-    };
-    const promise = api.post('/like', body, config(token));
-    return promise;
-}
+	const body = {
+		postId,
+		userId,
+	};
+	const promise = api.post('/like', body, config(token));
+	return promise;
+};
 
 const getLikes = (postId, token) => {
-    const promise = api.get(`/like/${postId}`, config(token));
-    return promise;
-}
+	const promise = api.get(`/like/${postId}`, config(token));
+	return promise;
+};
 
 const getTrendingHashtags = () => {
-    const promise = api.get('/hashtags/trending');
-    return promise;
-}
+	const promise = api.get('/hashtags/trending');
+	return promise;
+};
 
 const getUsers = (userQuery) => {
-    const promise = api.get(`/users?like=${userQuery}`);
-    return promise;
-}
+	const promise = api.get(`/users?like=${userQuery}`);
+	return promise;
+};
 
 const deletePost = (postId, token) => {
-    const promise = api.delete(`/posts/${postId}`, config(token));
-    return promise;
-}
+	const promise = api.delete(`/posts/${postId}`, config(token));
+	return promise;
+};
 
 const updatePost = (postId, token, description) => {
-    const promise = api.put(`/posts/${postId}`, {description}, config(token));
-    return promise;
-}
+	const promise = api.put(`/posts/${postId}`, { description }, config(token));
+	return promise;
+};
 
 const getUserPosts = (userId) => {
-    const promise = api.get(`/user/${userId}`);
-    return promise;
-}
+	const promise = api.get(`/user/${userId}`);
+	return promise;
+};
 
 const getFollowingsUser = (userId) => {
-    const promise = api.get(`/follows/${userId}`);
-    return promise;
-}
+	const promise = api.get(`/follows/${userId}`);
+	return promise;
+};
 
 const postFollow = (followerId, followingId, token) => {
-    const body = {
-        follower: followerId,
-        following: followingId
-    }
-    const promise = api.post('/follows', body, config(token));
-    return promise;
-}
+	const body = {
+		follower: followerId,
+		following: followingId,
+	};
+	const promise = api.post('/follows', body, config(token));
+	return promise;
+};
 
 const deleteFollow = (followingId, token) => {
-    const promise = api.delete(`/follows/${followingId}`, config(token));
-    return promise;
-}
+	const promise = api.delete(`/follows/${followingId}`, config(token));
+	return promise;
+};
 
 /*const example = (body ,token) => {
     const configAuth = config(token)
@@ -107,19 +105,19 @@ const deleteFollow = (followingId, token) => {
 }*/
 
 export {
-    signUp,
-    login,
-    getUserData,
-    publishPost,
-    getPosts,
-    likePost,
-    getLikes,
-    getTrendingHashtags,
-    deletePost,
-    updatePost,
-    getUsers,
-    getUserPosts,
-    getFollowingsUser,
-    postFollow,
-    deleteFollow,
-}
+	signUp,
+	login,
+	getUserData,
+	publishPost,
+	getPosts,
+	likePost,
+	getLikes,
+	getTrendingHashtags,
+	deletePost,
+	updatePost,
+	getUsers,
+	getUserPosts,
+	getFollowingsUser,
+	postFollow,
+	deleteFollow,
+};
